34f682b5a114e4185f519d4915ca6f98adca2f6a
Lucas Marcão <69424024+lucasmarcao@users.noreply.github.com>
2021-10-18 12:12:43 -0300

    Update naoseioq.py

--- slot_para_python/naoseioq.py
+++ slot_para_python/naoseioq.py
@@ -1,8 +1,23 @@
 from tkinter import *
+import tkinter as tk
 
-cp = Tk()
-cp.title("sera?")
-cp.geometry("400x400+500+200")
-cp.mainloop()
+window = tk.Tk()
+window.title("sera?")
+window.geometry("400x400+500+200")
+window.configure(bg="#808000")
 
 
+
+
+
+greeting = tk.Label(text="Hello, Tkinter")
+greeting.pack()
+greeting.configure(background="#00ffff",justify="right")
+
+botao = tk.Checkbutton(background="#800000",text="escolher",foreground="#ff00ff")
+botao.pack()
+
+botao2 = tk.Checkbutton(background="#800000",text="escolher",foreground="#ff00ff")
+botao2.pack()
+
+window.mainloop()
